2c175331473a1a0725f50e17bee4631766714aeb
kervint1 <s1f102302124@iniad.org>
2024-04-28 05:08:00 +0900

    関数追加

--- ocr.py
+++ ocr.py
@@ -25,6 +25,24 @@ def get_lp_account_name_message(file_path, left=55, top=10, right=200, bottom=10
 
     return slides_texts
 
+def get_lp_date_message(file_path, left=0, top=0, right=2000, bottom=2000):
+    prs = Presentation(file_path)
+    slides_texts = []  # 各スライドのテキストリストを格納するリスト
+
+    for slide in prs.slides:
+        slide_texts = []  # 現在のスライドのテキストを格納するリスト
+        for shape in slide.shapes:
+            if shape.shape_type == 13 and 32 < shape.left.pt < 37 and 143 < shape.top.pt < 147:  # 画像タイプ
+                image_stream = io.BytesIO(shape.image.blob)
+                image = Image.open(image_stream)
+                # 画像を指定された範囲でクロップ
+                cropped_image = image.crop((left, top, right, bottom))
+                text = pytesseract.image_to_string(cropped_image, lang='jpn')
+                slide_texts.append(text)
+        slides_texts.append(slide_texts)
+
+    return slides_texts
+
 # ファイルパスを指定して関数を呼び出し、結果を表示
-file_path = r"C:\Users\iniad\Documents\adpro\【事例資料】LOUIS VUITTON_LINE 公式アカウント_メッセージ配信_2024年1月以降.pptx"
-print(get_lp_account_name_message(file_path))
+file_path = r"【事例資料】LOUIS VUITTON_LINE 公式アカウント_メッセージ配信_2024年1月以降.pptx"
+print(get_lp_date_message(file_path))
